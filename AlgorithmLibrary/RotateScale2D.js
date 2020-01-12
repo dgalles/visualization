276aed8d20ec74db8b6794774512511f9baba837
Kostas Chatzikokolakis <kostas@chatzi.org>
2020-01-12 18:58:02 +0200

    use LF in the repo
    Files had mixed LF and CRLF endings, the standard git way is to use LF
    in the repo, git automatically converts to CRLF on windows.

--- AlgorithmLibrary/RotateScale2D.js
+++ AlgorithmLibrary/RotateScale2D.js
@@ -1,4 +1,4 @@
-﻿// Copyright 2011 David Galles, University of San Francisco. All rights reserved.
+// Copyright 2011 David Galles, University of San Francisco. All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without modification, areobjectVertexLocalPosition
 // permitted provided that the following conditions are met:
